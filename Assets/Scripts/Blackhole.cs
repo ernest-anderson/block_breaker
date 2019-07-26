894084e4e0d65e9c69f40c11a02784887cd7307b
ernest-anderson <ernest.anderson.hutasoit@gmail.com>
2019-07-26 14:28:14 +0700

    fixed bug in blackhole

--- Assets/Scripts/Blackhole.cs
+++ Assets/Scripts/Blackhole.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Blackhole : MonoBehaviour
 {
     Ball mainBall;
-    [SerializeField] Blackhole destinationBlackhole;
+    [SerializeField] Blackhole blackhole;
 
     private void Start()
     {
@@ -19,15 +19,20 @@ public class Blackhole : MonoBehaviour
     }
 
     private void WarpBall()
-    {        
+    {
         Vector2 velocityTweak = new Vector2(UnityEngine.Random.Range(0f, mainBall.randomFactor), UnityEngine.Random.Range(0f, mainBall.randomFactor));
         
-        Ball newBall = Instantiate(mainBall, destinationBlackhole.transform.position, destinationBlackhole.transform.rotation);        
-        newBall.GetComponent<Rigidbody2D>().velocity += velocityTweak;
-        destinationBlackhole.gameObject.SetActive(false);
+        Vector2 blackholePos = new Vector2(blackhole.transform.position.x, blackhole.transform.position.y);
+        mainBall.transform.position = blackholePos;
+        mainBall.GetComponent<Rigidbody2D>().velocity += velocityTweak;
+        DeactivateBlackhole();
 
-        Destroy(mainBall);
-        //Destroy(GameObject.FindObjectOfType<Ball>());
-        //destinationBlackhole.gameObject.SetActive(true);
+        Invoke("ActivateBlackhole", 0.25f);
+    }
+    private void DeactivateBlackhole() {
+        blackhole.gameObject.SetActive(false);
+    }
+    private void ActivateBlackhole() {
+        blackhole.gameObject.SetActive(true);
     }
 }
